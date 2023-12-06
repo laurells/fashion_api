317c27d6802bd1396ab2d3efcf0579af7c1e7b8b
Laurels Echichinnwo <laurels.echichinwo@stu.cu.edu.ng>
2023-12-06 20:03:25 +0000

    update

--- prisma/singleton.ts
+++ prisma/singleton.ts
@@ -1,7 +1,6 @@
 import { PrismaClient } from "@prisma/client";
 import { mockDeep, mockReset } from "jest-mock-extended";
 import { DeepMockProxy } from "jest-mock-extended/lib/cjs/Mock";
-
 import prisma from "./client";
 
 require("jest-sorted");
